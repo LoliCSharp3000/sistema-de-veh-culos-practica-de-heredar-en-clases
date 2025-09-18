06753ee43f88e7e71ab243cc8726ad6ff86df60f
Makim <mkorfom@g.educaand.es>
2025-09-18 13:40:17 +0200

    first

---
+++ src/Javacito.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner;
+
+public class Javacito {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int edad = sc.nextInt();
+        System.out.println("Hello" + edad);
+        sc.close();
+    }
+}
